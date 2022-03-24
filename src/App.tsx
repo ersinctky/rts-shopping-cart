efdb12c6ba84ab9e337fd5adc067514606c7ff30
Ersin Çatkaya <ersin_ctky@hotmail.com>
2022-03-24 15:08:03 +0300

    Cart & CartItem folder created

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import Badge from '@material-ui/core/Badge';
 // Styles
 import { Wrapper, StyledButton } from './App.styles';
+import Cart from './Cart/Cart';
 // Types
 export type CartItemType = {
   id: number;
@@ -42,7 +43,11 @@ const App = () => {
   return (
     <Wrapper>
       <Drawer anchor="right" open={cartOpen} onClose={() => setCartOpen(false)}>
-        Cart goes here
+        <Cart
+          cartItems={cartItems}
+          addToCart={handleAddToCart}
+          removeFromCart={handleRemoveFromCart}
+        />
       </Drawer>
       <StyledButton onClick={() => setCartOpen(true)}>
         <Badge badgeContent={getTotalItems(cartItems)} color="error">
